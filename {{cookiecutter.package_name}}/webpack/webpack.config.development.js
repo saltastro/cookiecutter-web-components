94e7695bf347e03a6d1196d15c8c3f79e4154f36
Christian Hettlage <hettlage@saao.ac.za>
2018-10-05 00:10:12 +0200

    serve page with web component

--- {{cookiecutter.package_name}}/webpack/webpack.config.development.js
+++ {{cookiecutter.package_name}}/webpack/webpack.config.development.js
@@ -1,9 +1,33 @@
 const merge = require('webpack-merge')
+const MiniHtmlWebpackPlugin = require('mini-html-webpack-plugin')
+const {
+  generateCSSReferences,
+  generateJSReferences
+} = MiniHtmlWebpackPlugin
+
+const html = ({ css, js, publicPath }) =>
+        `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+${generateCSSReferences(css, publicPath)}
+${generateJSReferences(js, publicPath)}
+</head>
+<body>
+<{{ cookiecutter.component_tag }}></{{ cookiecutter.component_tag }}>
+</body>
+</html>`
 
 const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
   mode: 'development',
 
-  devtool: 'cheap-module-source-map'
+  devtool: 'cheap-module-source-map',
+
+  plugins: [
+    new MiniHtmlWebpackPlugin({
+      template: html
+    })
+  ]
 })
