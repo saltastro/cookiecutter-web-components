0b48dea62004284ccf72ce729efcd9ecc43f3770
Christian Hettlage <hettlage@saao.ac.za>
2018-10-04 16:26:35 +0200

    install node modules are project is generated

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+npm install --save-dev \
+        jest \
+        babel-jest \
+        babel-core@^7.0.0-bridge.0 \
+        @babel/core \
+        @babel/preset-env \
+        regenerator-runtime \
+        eslint \
+        eslint-config-standard \
+        eslint-plugin-standard \
+        eslint-plugin-promise \
+        eslint-plugin-import \
+        eslint-plugin-node \
+        eslint-plugin-jest \
+        puppeteer \
+        jest-puppeteer \
+        express \
+        webpack \
+        webpack-merge
