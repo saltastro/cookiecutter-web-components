afc7a833b0838623e3382825361becc48835d4fa
Christian Hettlage <hettlage@saao.ac.za>
2018-10-05 22:17:30 +0200

    only include license file for MIT license

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
- npm install --save-dev \
-        jest \
-        babel-core@^7.0.0-bridge.0 \
-        @babel/core \
-        @babel/preset-env \
-        babel-loader \
-        babel-jest \
-        regenerator-runtime \
-        eslint \
-        eslint-config-standard \
-        eslint-plugin-standard \
-        eslint-plugin-promise \
-        eslint-plugin-import \
-        eslint-plugin-node \
-        eslint-plugin-jest \
-        puppeteer \
-        jest-puppeteer \
-        express \
-        webpack \
-        webpack-cli \
-        webpack-dev-server \
-        webpack-merge \
-        mini-html-webpack-plugin
+# include license filre only if MIT license is chosen
+
+if [ "MIT" != "{{ cookiecutter.license }}" ]
+then
+  echo "{{ cookiecutter.license }}"
+  rm LICENSE
+fi
+
+# install the required Node packages
+
+npm install --save-dev \
+    jest \
+    babel-core@^7.0.0-bridge.0 \
+    @babel/core \
+    @babel/preset-env \
+    babel-loader \
+    babel-jest \
+    regenerator-runtime \
+    eslint \
+    eslint-config-standard \
+    eslint-plugin-standard \
+    eslint-plugin-promise \
+    eslint-plugin-import \
+    eslint-plugin-node \
+    eslint-plugin-jest \
+    puppeteer \
+    jest-puppeteer \
+    express \
+    webpack \
+    webpack-cli \
+    webpack-dev-server \
+    webpack-merge \
+    mini-html-webpack-plugin
